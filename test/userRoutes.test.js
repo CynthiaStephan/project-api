af73ccad18c0e8920e7257bc89cf97717a92e2ba
CynthiaStephan <cynthia.stephan@institutsolacroup.com>
2024-11-06 17:09:55 +0100

    up

--- test/userRoutes.test.js
+++ test/userRoutes.test.js
@@ -7,4 +7,18 @@ app.use(express.json());
 app.use(userRoutes);
 
 
-describe('GET /api/users/:id', () => )
+describe('GET /api/users/:id', () => {
+    it('Should get on user', async () => {
+        const userId = 13; 
+        
+        const response = await request(app)
+            .get(`/api/users/${userId}`)
+            .expect(200)
+        
+            expect(response.body).toHaveProperty('id');
+            expect(response.body.name).toBe(userData.name);
+            expect(response.body.email).toBe(userData.email);
+            expect(response.body.age).toBe(userData.age);
+
+    });
+});
